1614caab07653c9c1627759cfee2917a1bf21781
Pauline Ribeyre <ribeyre@uchicago.edu>
2019-08-28 14:18:10 -0500

    feat(qa): login through staging STAGE

--- src/config.js
+++ src/config.js
@@ -1,12 +1,13 @@
 // Backend API URL. By default, use QA backend
 const apiHost = process.env.REACT_APP_API_HOST || 'https://qa-access-api.planx-pla.net';
 
-const authHost = 'https://gen3.datastage.io';
+// Implicit client to login. By default, assume we log in through staging
+const authHost = process.env.REACT_APP_AUTH_HOST || 'https://staging.datastage.io';
+const clientId = process.env.REACT_APP_CLIENT_ID;
+const redirectUrl = process.env.REACT_APP_REDIRECT_URL;
 const oauthResponseType='id_token+token';
 const oauthScope='openid+user'
 const authUrl = `${authHost}/user/oauth2/authorize`;
-const clientId = process.env.REACT_APP_CLIENT_ID;
-const redirectUrl = process.env.REACT_APP_REDIRECT_URL;
 const tokenPath = 'stageToken';
 const userApi =`${authHost}/user/user`;
 const loginOptionsUrl = `${authHost}/user/login`;
