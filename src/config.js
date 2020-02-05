72dcdbdc90775a15d4bf4516a13f4859c4baa8ed
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2020-02-05 14:08:01 -0600

    feat(stage-url-change): updated url for stage

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 const apiHost = process.env.REACT_APP_API_HOST || 'https://qa-access-api.planx-pla.net';
 
 // Implicit client to login. By default, assume we log in through staging
-const authHost = process.env.REACT_APP_AUTH_HOST || 'https://staging.datastage.io';
+const authHost = process.env.REACT_APP_AUTH_HOST || 'https://staging.gen3.biodatacatalyst.nhlbi.nih.gov';
 const clientId = process.env.REACT_APP_CLIENT_ID;
 const redirectUrl = process.env.REACT_APP_REDIRECT_URL;
 const oauthResponseType='id_token+token';
